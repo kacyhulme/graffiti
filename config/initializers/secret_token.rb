aa9115ec7c839e04cb07a2c687ce9203c4cf5ad3
Kacy Hulme <kacyhulme@gmail.com>
2013-06-21 11:46:17 -0500

    first migration

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Graffiti::Application.config.secret_key_base = '4ef5bfb199831bffa80c5a7770a3cb26f459565d545d4e8db0051d2b4331fcf05a82ecfd63aececacf5e1a9133ae72dce604a07f891001e5049f86489d0d1cca'
